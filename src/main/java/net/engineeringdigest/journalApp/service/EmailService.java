2aae3d144647c88a8dfdf5137a35b2e7309386ec
Rajeev Ranjan Prasad <rajeevranjanprasad@Rajeevs-MacBook-Air.local>
2025-10-16 15:12:12 +0530

    fixing the error for send email

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -1,11 +1,13 @@
 package net.engineeringdigest.journalApp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+   @Autowired
      private JavaMailSender javaMailSender;
      
      public void sendMail(String to,String subject,String body){
